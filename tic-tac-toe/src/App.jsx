ee15ecb672017502ce62f3aff9c3671a37114b8a
Deji Toki <adedeji.toki@outlook.com>
2023-02-23 11:42:39 -0800

    Integrate Server for tic tac logic, work on chat UI

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -2,68 +2,86 @@ import './App.css'
 import Square from './components/Square'
 import { useEffect, useState } from 'react'
 import { CIRCLE, CROSS, EMPTY } from './constants';
+import io from 'socket.io-client';
+import ChatBody from './components/ChatBody';
+import ChatFooter from './components/ChatFooter';
+import useScrollBlock from './useScrollBlock';
+
+const socket = io("http://localhost:4000");
 
 const App = () => {
-
   /**
    * currentPlayer: CIRCLE
    */
-  const [currentPlayer, setCurrentPlayer] = useState(CIRCLE)
+  // const [currentPlayer, setCurrentPlayer] = useState(CIRCLE)
+  const [myPlayer, setMyPlayer] = useState(null);
   const [positions, setPositions] = useState([
       EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY
   ]);
   const [winner, setWinner] = useState(null);
-
-  const winningCombinations = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], 
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], 
-    [0, 4, 8], [2, 4, 6]
-  ]
+  const [gameStarted, setGameStarted] = useState(false);
 
   const handlePlayerMove = (position) => {
-    const updatedPositions = [...positions];
-    updatedPositions[position] = currentPlayer;
-    setPositions(updatedPositions);
-    setCurrentPlayer(currentPlayer === CIRCLE ? CROSS : CIRCLE);
-  };
-
-  const checkGameWinner = () => {
-    for (const i in winningCombinations) {
-      const [a, b, c] = winningCombinations[i];
-      if (positions[a] === positions[b] && positions[b] === positions[c] && positions[a] !== EMPTY) {
-        return positions[a];
-      }
+    if (gameStarted === false) {
+      alert('Game has not started yet. Please wait for the other player to join.');
+      return;
+    }
+    if (myPlayer === null) {
+      return;
     }
 
-    if (positions.every((position) => position !== EMPTY)) {
-      return EMPTY;
-    }
-    return null;
+    socket.emit('playerMove', position, myPlayer);
   };
+
+  const onConnection = () => {
+    console.log('Connected to server');
+    // retrieve current game state
+    socket.emit('getGameState');
+    // listen for game state
+  }
 
   useEffect(() => {
-    const winner = checkGameWinner();
-    setWinner(winner);
-  });
+    socket.on('gameUpdate', (newPositions, currentPlayer, gameWinner, gameInProgress) => {
+      console.log('gameUpdate', newPositions, currentPlayer);
+      setPositions(newPositions);
+      // setCurrentPlayer(currentPlayer);
+      setWinner(gameWinner);
+      setGameStarted(gameInProgress);
+    })
+    socket.on('gameStarted', (booleanVal) => {
+      setGameStarted(booleanVal);
+    })
+    socket.on('myPlayerUpdate', (player) => {
+      console.log('myPlayerUpdate', player);
+      setMyPlayer(player);
+    })
+  }, []);
 
   return (
     <div className="App">
-      <h1 className='app_heading'>Tic Tac Toe</h1>
-
-      <div className="game_board">
-        {
-          positions.map((position, index) => (
-             <Square 
-              key={index * 430} 
-              position={index} 
-              value = {position}
-              handlePlayerMove = {handlePlayerMove}/>
-          ))
-        }
+      <div className="game_section">
+        <h1 className='app_heading'>Tic Tac Toe</h1>
+        <div className="game_board">
+          {
+            positions.map((position, index) => (
+              <Square 
+                key={index * 430} 
+                position={index} 
+                value = {position}
+                handlePlayerMove = {handlePlayerMove}/>
+            ))
+          }
+        </div>
+        {/* Game Results */}
+        {winner}
+      </div>
+      <div className="game_chat">
+        {/* <h2>Cat</h2> */}
+        <ChatBody/>
+        <ChatFooter/>
       </div>
 
-      {/* Game Results */}
-      {winner}
+      
     </div>
   )
 }
